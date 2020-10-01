3e560ac73cf3d8377a1841a449b181dea4c11245
¨Julian Colina <colina.julian07@gmail.com>
2020-09-30 22:31:46 -0500

    primer cambio

---
+++ led_luces_navidad.ino/led_luces_navidad.ino.ino
@@ -0,0 +1,136 @@
+void setup()
+{
+  pinMode(22, OUTPUT);
+pinMode(32, OUTPUT);
+pinMode(42, OUTPUT);
+pinMode(24, OUTPUT);
+pinMode(34, OUTPUT);
+pinMode(44, OUTPUT);
+pinMode(26, OUTPUT);
+pinMode(36, OUTPUT);
+pinMode(46, OUTPUT);
+pinMode(28, OUTPUT);
+pinMode(38, OUTPUT);
+pinMode(48, OUTPUT);
+pinMode(30, OUTPUT);
+pinMode(40, OUTPUT);
+pinMode(50, OUTPUT);
+  digitalWrite(22, HIGH);
+  delay(1000);
+  digitalWrite(32, HIGH);
+  delay(1000);
+  digitalWrite(42, HIGH);
+  delay(1000);
+  digitalWrite(22, HIGH);
+  digitalWrite(32, HIGH);
+  digitalWrite(42, HIGH);
+  delay(1000);
+  digitalWrite(22, LOW);
+  digitalWrite(32, LOW);
+  digitalWrite(42, LOW);
+  delay(1000);
+  digitalWrite(24, HIGH);
+  delay(1000);
+  digitalWrite(34, HIGH);
+  delay(1000);
+  digitalWrite(44, HIGH);
+  delay(1000);
+  digitalWrite(24, HIGH);
+  digitalWrite(34, HIGH);
+  digitalWrite(44, HIGH);
+  delay(1000);
+  digitalWrite(24, LOW);
+  digitalWrite(34, LOW);
+  digitalWrite(44, LOW);
+  delay(1000);
+  digitalWrite(26, HIGH);
+  delay(1000);
+  digitalWrite(36, HIGH);
+  delay(1000);
+  digitalWrite(46, HIGH);
+  delay(1000);
+  digitalWrite(26, HIGH);
+  digitalWrite(36, HIGH);
+  digitalWrite(46, HIGH);
+  delay(1000);
+  digitalWrite(26, LOW);
+  digitalWrite(36, LOW);
+  digitalWrite(46, LOW);
+  delay(1000);
+  digitalWrite(28, HIGH);
+  delay(1000);
+  digitalWrite(38, HIGH);
+  delay(1000);
+  digitalWrite(48, HIGH);
+  delay(1000);
+  digitalWrite(28, HIGH);
+  digitalWrite(38, HIGH);
+  digitalWrite(48, HIGH);
+  delay(1000);
+  digitalWrite(28, LOW);
+  digitalWrite(38, LOW);
+  digitalWrite(48, LOW);
+  delay(1000);
+  digitalWrite(30, HIGH);
+  delay(1000);
+  digitalWrite(40, HIGH);
+  delay(1000);
+  digitalWrite(50, HIGH);
+  delay(3000);
+  digitalWrite(22, HIGH);
+  digitalWrite(50, HIGH);
+  delay(1000);
+  digitalWrite(22, LOW);
+  digitalWrite(50, LOW);
+  delay(1000);
+  digitalWrite(24, HIGH);
+  digitalWrite(48, HIGH);
+  delay(1000);
+  digitalWrite(24, LOW);
+  digitalWrite(48, LOW);
+  delay(1000);
+  digitalWrite(26, HIGH);
+  digitalWrite(46, HIGH);
+  delay(1000);
+  digitalWrite(26, LOW);
+  digitalWrite(46, LOW);
+  delay(1000);
+  digitalWrite(28, HIGH);
+  digitalWrite(44, HIGH);
+  delay(1000);
+  digitalWrite(28, LOW);
+  digitalWrite(44, LOW);
+  delay(1000);
+  digitalWrite(30, HIGH);
+  digitalWrite(42, HIGH);
+  delay(1000);
+  digitalWrite(30, LOW);
+  digitalWrite(42, LOW);
+  delay(1000);
+  digitalWrite(32, HIGH);
+  digitalWrite(40, HIGH);
+  delay(1000);
+  digitalWrite(32, LOW);
+  digitalWrite(40, LOW);
+  delay(1000);
+  digitalWrite(34, HIGH);
+  digitalWrite(38, HIGH);
+  delay(1000);
+  digitalWrite(34, LOW);
+  digitalWrite(38, LOW);
+  delay(1000);
+  digitalWrite(36, HIGH);
+  delay(2000);
+  digitalWrite(36, LOW);
+  delay(1000);
+
+}
+
+
+void loop()
+{
+
+
+}
+  
+    
